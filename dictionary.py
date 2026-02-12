ff1a7caaaa8417d593e1d0e315f54b47b9a11f47
ordinary-ryan <ryanl422@nycstudents.net>
2026-02-12 10:13:24 -0500

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+sam_shop = []
+    seeds=
+    {
+        "name" : "mango seed",
+        "price" : 100000,
+        "rarity" : "mythical"
+        "description" : "A mythical fruit that has a 1.25 to 1.5 percent chance of stocking in the shop. Spawns a fruit every 2 minutes after maturity."
+
+
+
+    }
