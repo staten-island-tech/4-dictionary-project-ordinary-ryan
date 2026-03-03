5e029c7d5f5423edd41dfcadba68ed11a3d296a9
ordinary-ryan <ryanl422@nycstudents.net>
2026-03-03 10:17:16 -0500

    Update testpractice.py

--- testpractice.py
+++ testpractice.py
@@ -10,7 +10,7 @@
 occupied (5, "c.ccc", "c.c.c")
  """
 
-def lang(sent):
+""" def lang(sent):
         s = 0
         t = 0
         for i in sent:
@@ -24,13 +24,33 @@ def lang(sent):
         else:
             print("this is likely english")
 
-lang("The red cat sat on the mat. Why are you so sad cat? Don't ask that")
+lang("The red cat sat on the mat. Why are you so sad cat? Don't ask that") """
 
-""" def lang(sent):
-    s = 0
-    t = 0
+
+def lang(sent):
+    h= 0
     for i in sent:
-        if i == "s" or i == "S":
-               s += 1
-        elif i == "t" or i == "T"
- """
+        if i == "H":
+            h += 0
+        elif i == "O" and i == "H":
+            h += 0
+        elif i == "O" and i == "H" and i == "N":
+            h +=0 
+        elif i == "O" and i == "H" and i == "N" and i == "I":
+            h += 1
+    honi_count = h
+    print(honi_count)
+    
+lang("PROHODNIHODNIK")
+""" for i in sent:
+        if i == "H":
+            h += 1
+        elif i == "O" :
+            o += 1
+        elif i == "N" :
+            n += 1
+        elif i == "I":
+            e += 1
+        
+            honi_count = (h + o + n + e) / 4
+        print(honi_count) """
